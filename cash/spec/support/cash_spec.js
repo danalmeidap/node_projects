12d2b147530e5a7c327261451065ea7e70c8c2f1
danalmeidap <dbatistadealmeida23@gmail.com>
2021-02-24 22:46:08 -0300

    greedy algorithm done

--- cash/spec/support/cash_spec.js
+++ cash/spec/support/cash_spec.js
@@ -20,40 +20,48 @@ describe("Cash greedy algorithm tests", function() {
     });
 
 
+    
     it("Should recieves one quarter", ()=>{
       let change = cash.change_owned(25);
       expect(change).toBe(1);
     });
+
 
     it("Should recieves two coins", ()=>{
       let change = cash.change_owned(15);
       expect(change).toBe(2);
     });
 
+
     it("Should recieve four coins ", ()=>{
       let change = cash.change_owned(41);
       expect(change).toBe(4);
     });
+
 
     it("Should recieves seven coins", ()=>{
       let change = cash.change_owned(160);
       expect(change).toBe(7);
     });
 
+
     it("Should recieves ninety two coins", ()=>{
       let change = cash.change_owned(2300);
       expect(change).toBe(92);
     });
+
 
     it("Should recieves eighteen coins", ()=>{
       let change = cash.change_owned(420);
       expect(change).toBe(18);
     });
 
+
     it("Should recieves negative value", ()=>{
       let change = cash.change_owned(-1);
       expect(change).toBe("invalid number");
     });
+
 
     it("Should recies a non-numeric valor", ()=>{
       let change = cash.change_owned("foo");
