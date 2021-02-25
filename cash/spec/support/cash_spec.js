077d78978e20af44e2d56e5cf398232a774c5b31
danalmeidap <dbatistadealmeida23@gmail.com>
2021-02-24 22:13:37 -0300

    base cases

--- cash/spec/support/cash_spec.js
+++ cash/spec/support/cash_spec.js
@@ -1,7 +1,53 @@
 var cash = require ("../../src/index.js");
 describe("Cash greedy algorithm tests", function() {
-    it("should welcome user", function() {
-      let hello_message = cash.hello();
-      expect(hello_message).toBe("Ola mundo!");
+    
+
+    it("Recieves the minimum value", ()=>{
+      let change = cash.change_owned(1);
+      expect(change).toBe(1);
     });
+
+
+    it("Should recieves one nickel", ()=>{
+      let change = cash.change_owned(5);
+      expect(change).toBe(1);
+    });
+
+
+    it("Should recieves one dimes", ()=>{
+      let change = cash.change_owned(10);
+      expect(change).toBe(1);
+    });
+
+
+    it("Should recieves one quarter", ()=>{
+      let change = cash.change_owned(25);
+      expect(change).toBe(1);
+    });
+
+    it("Should recieves two coins", ()=>{
+      let change = cash.change_owned(15);
+      expect(change).toBe(2);
+    });
+
+    it("Should recieve four coins ", ()=>{
+      let change = cash.change_owned(41);
+      expect(change).toBe(4);
+    });
+
+    it("Should recieves seven coins", ()=>{
+      let change = cash.change_owned(160);
+      expect(change).toBe(7);
+    });
+
+    it("Should recieves ninety two coins", ()=>{
+      let change = cash.change_owned(2300);
+      expect(change).toBe(92);
+    });
+
+    it("Should recieves eighteen coins", ()=>{
+      let change = cash.change_owned(420);
+      expect(change).toBe(18);
+    });
+
   });
--- cash/src/index.js
+++ cash/src/index.js
@@ -1,5 +1,33 @@
-var hello = function()
+var change_owned = function(value)
 {
-    return "Ola mundo!";
+    var counter = 0;
+    while (value > 0)
+    {
+        if (value >= 25)
+        {
+            value -= 25;
+            counter ++;
+        }
+        
+        else if (value >= 10)
+        {
+            value -= 10;
+            counter ++;
+        }
+    
+        else if (value >= 5)
+        {
+            value -= 5;
+            counter ++;
+        }
+    
+        else if (value >= 1)
+        {
+            value -= 1;
+            counter ++;
+        }   
+
+    }
+    return counter;
 }
-exports.hello = hello;
+exports.change_owned = change_owned;
